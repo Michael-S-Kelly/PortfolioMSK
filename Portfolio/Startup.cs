05ad46327f008c86d8b00e7a902216b0297096c0
Michael Kelly <mike.s.kelly@icloud.com>
2020-03-12 21:18:20 -0700

    Last Initial fix

--- Portfolio/Startup.cs
+++ Portfolio/Startup.cs
@@ -16,6 +16,7 @@ namespace Portfolio
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,11 +31,20 @@ namespace Portfolio
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            /*
+            app.UseEndpoints(endpoints =>
+            {
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Hello World!");
                 });
             });
+            */
         }
     }
 }
